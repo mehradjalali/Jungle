9d755b19323c8e48e31c9b7c18a334483800d8f1
mehrad <mehradj@yahoo.com>
2022-06-07 22:59:00 +0430

    second part

--- main.cpp
+++ main.cpp
@@ -23,4 +23,22 @@ int main() {
     for (int i = 0; i < 1000; i++)
         jungle.movement();
     cout << "total falls: " << jungle.getTotalFall() << endl;
+    //SECOND PART
+    cout << "enter the number of animals(three animals): ";
+    cin >> count;
+    Jungle jungle2 = Jungle(count);
+    i = 0;
+    for (; i < count / 3; i += 2){
+        WildAnimal *n = new WildAnimal();
+        jungle2.AddAnimal((Animal*) n);
+        DomesticAnimal *m = new DomesticAnimal();
+        jungle2.AddAnimal((Animal*) m);
+    }
+    for (; i < count; i++){
+        Pet *n = new Pet();
+        jungle2.AddAnimal((Animal*) n);
+    }
+    for (int i = 0; i < 1000; i++)
+        jungle2.movement();
+    cout << "total falls: " << jungle2.getTotalFall() << endl;
 }
