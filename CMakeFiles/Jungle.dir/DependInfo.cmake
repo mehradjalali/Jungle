22a4e9f6055edfd5be410689d40ab42e820863ff
mehrad <mehradj@yahoo.com>
2022-06-07 22:59:19 +0430

    cmake

---
+++ CMakeFiles/Jungle.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amin/Documents/ap/jungle/Jungle/Animal.cpp" "/home/amin/Documents/ap/jungle/Jungle/CMakeFiles/Jungle.dir/Animal.cpp.o"
+  "/home/amin/Documents/ap/jungle/Jungle/DomesticAnimal.cpp" "/home/amin/Documents/ap/jungle/Jungle/CMakeFiles/Jungle.dir/DomesticAnimal.cpp.o"
+  "/home/amin/Documents/ap/jungle/Jungle/Jungle.cpp" "/home/amin/Documents/ap/jungle/Jungle/CMakeFiles/Jungle.dir/Jungle.cpp.o"
+  "/home/amin/Documents/ap/jungle/Jungle/Pet.cpp" "/home/amin/Documents/ap/jungle/Jungle/CMakeFiles/Jungle.dir/Pet.cpp.o"
+  "/home/amin/Documents/ap/jungle/Jungle/WildAnimal.cpp" "/home/amin/Documents/ap/jungle/Jungle/CMakeFiles/Jungle.dir/WildAnimal.cpp.o"
+  "/home/amin/Documents/ap/jungle/Jungle/main.cpp" "/home/amin/Documents/ap/jungle/Jungle/CMakeFiles/Jungle.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
