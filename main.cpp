9aab8ab57aa6e06d8fa24ecd0aba44b79073913b
mehrad <mehradj@yahoo.com>
2022-06-07 22:58:48 +0430

    first part

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "Jungle.h"
+#include "WildAnimal.h"
+#include "DomesticAnimal.h"
+#include "Pet.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int count;
+    cout << "enter the number of animals(two animals): ";
+    cin >> count;
+    Jungle jungle = Jungle(count);
+    int i = 0;
+    for (; i < count / 2; i++){
+        WildAnimal *n = new WildAnimal();
+        jungle.AddAnimal((Animal*) n);
+    }
+    for (; i < count; i++){
+        DomesticAnimal *n = new DomesticAnimal();
+        jungle.AddAnimal((Animal*) n);
+    }
+    for (int i = 0; i < 1000; i++)
+        jungle.movement();
+    cout << "total falls: " << jungle.getTotalFall() << endl;
 }
